7ea1c55a51e0eb5b984824be4462cd15ad01ded6
Minyeong Jeong <megormin@gmail.com>
2025-01-18 21:11:52 +0100

    wip

--- src/components/carts/OrderCart.tsx
+++ src/components/carts/OrderCart.tsx
@@ -1,27 +1,58 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FormEvent } from "react";
-import { TCart } from "@/utils/supabase/Types";
+import { FormEvent, useState } from "react";
 import { supabase } from "@/utils/supabase/SetupSupabase";
+import { TOrder } from "@/utils/supabase/Types";
 
 const OrderCart = () => {
-  async function orderCart(e: FormEvent<HTMLFormElement>) {
+  const [orders, setOrders] = useState<TOrder[] | []>([]);
+  function orderCart(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    const { cart_id } = e.currentTarget;
+    const { customer_id } = e.currentTarget;
 
-    const cart: Pick<TCart, "customer_id"> = {
-      customer_id: cart_id.value,
-    };
+    async function addOrders() {
+      const { error, data } = await supabase
+        .from("orders")
+        .insert({ customer_id: customer_id.value })
+        .select();
 
-    const { error } = await supabase.from("carts").insert(cart);
-
-    if (error) {
-      console.error(error);
-      alert("this customer ID already exists or does not exist 🙏");
+      if (error) {
+        console.error(error);
+      }
+      if (data) {
+        setOrders(data);
+      }
     }
 
-    cart_id.value = "";
+    async function emptyCart() {
+      await supabase
+        .from("cart_items")
+        .delete()
+        .eq("customer_id", customer_id.value);
+    }
+
+    // async function addOrderItems() {
+    //   if (orders) {
+    //     const { error } = await supabase
+    //       .from("order_items")
+    //       .insert({
+    //         order_id: orders[0].id,
+    //         item_name: "bose",
+    //         item_price: "777",
+    //         item_quantity: "2",
+    //       });
+
+    //     if (error) {
+    //       console.error(error);
+    //     }
+    //   }
+    // }
+
+    addOrders();
+    // addOrderItems();
+    emptyCart();
+    customer_id.value = "";
   }
 
   return (
@@ -29,9 +60,9 @@ const OrderCart = () => {
       <h2 className="font-semibold text-xl text-yellow-100">Order Cart </h2>
       <form onSubmit={orderCart} className="flex gap-4">
         <Input
-          name="cart_id"
+          name="customer_id"
           type="number"
-          placeholder="Cart ID"
+          placeholder="Customer ID"
           min={0}
           className="w-40"
         />
