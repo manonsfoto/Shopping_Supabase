295435aa8b108c8c794cfa80b23a0063bef8ffb5
Minyeong Jeong <megormin@gmail.com>
2025-01-18 14:58:47 +0100

    OrderCart wip

--- src/components/carts/OrderCart.tsx
+++ src/components/carts/OrderCart.tsx
@@ -1,10 +1,42 @@
-import { Button } from "../ui/button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FormEvent } from "react";
+import { TCart } from "@/utils/supabase/Types";
+import { supabase } from "@/utils/supabase/SetupSupabase";
 
 const OrderCart = () => {
+  async function orderCart(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const { cart_id } = e.currentTarget;
+
+    const cart: Pick<TCart, "customer_id"> = {
+      customer_id: cart_id.value,
+    };
+
+    const { error } = await supabase.from("carts").insert(cart);
+
+    if (error) {
+      console.error(error);
+      alert("this customer ID already exists or does not exist 🙏");
+    }
+
+    cart_id.value = "";
+  }
+
   return (
     <>
-      {" "}
-      <Button variant="outline">Order</Button>
+      <h2 className="font-semibold text-xl text-yellow-100">Order Cart </h2>
+      <form onSubmit={orderCart} className="flex gap-4">
+        <Input
+          name="cart_id"
+          type="number"
+          placeholder="Cart ID"
+          min={0}
+          className="w-40"
+        />
+        <Button variant="outline">Order</Button>
+      </form>
     </>
   );
 };
